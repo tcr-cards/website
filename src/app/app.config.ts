d9b74a143be647eb044fa8e1b3d9eaa148bf6988
JuanCGit <juancasanovagago@gmail.com>
2025-12-03 19:44:12 +0100

    feat: add translations handling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideTranslateService, TranslateService } from '@ngx-translate/core';
+
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: '/assets/i18n/',
+        suffix: '.json'
+      }),
+      fallbackLang: 'en',
+      lang: 'en'
+    }),
+    provideAppInitializer(() => {
+      const translate = inject(TranslateService);
+      translate.use(localStorage.getItem('lang') ?? translate.getBrowserLang() ?? 'en');
+      localStorage.setItem('lang', translate.getCurrentLang());
+    }),
   ]
 };
